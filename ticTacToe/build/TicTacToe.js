0df5d61527c36dcdb16e7343647108fb04d088d7
Harold C <hcabalic@gmail.com>
2016-02-15 00:56:31 -0800

    Create TicTacToe.js

---
+++ ticTacToe/build/TicTacToe.js
@@ -0,0 +1,176 @@
+var TicTacToeBoard = React.createClass({displayName: "TicTacToeBoard",
+  getInitialState: function() {
+    return {
+      headline: 'Tic Tac Toe',
+      games: []
+    };
+  },
+
+  add: function() {
+    // adds a new game
+    var games = this.state.games;
+
+    // each game needs a unique ID
+    games.push({
+      id: this.nextId()
+    });
+
+    this.setState({games: games});
+  },
+
+  eachGame: function(game, i) {
+    return (
+      React.createElement(Game, {key: game.id, index: i})
+      );
+  },
+
+  nextId: function() {
+    // keeps track of mmost recent ID
+    this.uniqueId = this.uniqueId || 0;
+    return this.uniqueId++;
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", {className: "container"}, 
+        React.createElement("h1", null, this.state.headline), 
+        this.state.games.map(this.eachGame), 
+        React.createElement("div", null, 
+          React.createElement("button", {id: "btn-add", className: "btn btn-sm btn-success glyphicon glyphicon-plus", onClick: this.add})
+        )
+      )
+    )
+  }
+});
+
+var Game = React.createClass({displayName: "Game",
+  getInitialState: function() {
+    return {
+      currentPiece: 'X',
+      winner: '',
+      board: [
+        ['','',''],
+        ['','',''],
+        ['','','']
+      ]
+    };
+  },
+
+  componentDidMount: function(){
+    $(this.getDOMNode()).draggable();
+  },
+
+  play: function(x, y) {
+    var board = this.state.board;
+
+    // place piece
+    board[x][y] = this.state.currentPiece;
+
+    // set state and check winner
+    this.setState({board: board}, this.checkWinner.bind(this, x, y));
+
+    // disable the space on the board
+    //this.disableCell();
+
+  },
+
+  disableCell: function() {
+    // TODO
+    $('button[data-tag="' + tag + '"]').prop('disabled', true);
+  },
+
+  _swapPiece: function () {
+    // changes the current player piece
+    if (this.state.currentPiece === 'X') {
+      this.setState({currentPiece: 'O'});
+    } else {
+      this.setState({currentPiece: 'X'});
+    }
+  },
+
+  checkWinner: function(x, y) {
+    var board = this.state.board;
+
+    // check for winner
+    if (this._checkRow(x) || this._checkColumn(y) || this._checkDiagonal()) {
+      this._displayWinner();
+    } else {
+      this._swapPiece();
+    }
+  },
+
+  _checkRow: function(x) {
+    var board = this.state.board;
+
+    // checks all spaces in current row
+    for (var i = 0; i < board.length; i++) {
+      if (board[x][i] !== this.state.currentPiece) {
+        return false;
+      }
+    }
+
+    return true;
+  },
+
+  _checkColumn: function(y) {
+    var board = this.state.board;
+
+    // checks all spaces in current column
+    for (var j = 0; j < board.length; j++) {
+      if (board[j][y] !== this.state.currentPiece) {
+        return false;
+      }
+    }
+
+    return true;
+  },
+
+  _checkDiagonal: function() {
+    var board = this.state.board;
+    var winner = this.state.currentPiece;
+
+    if (board[1][1] === winner) {
+      // check first diagonal
+      if (board[0][0] === winner && board[1][1] === winner) {
+        return true;
+      }
+
+      // check second diagonal
+      if (board[0][2] === winner && board[2][0] === winner) {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  },
+
+  _displayWinner: function() {
+    alert('you win');
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", {className: "game"}, 
+        React.createElement("table", {className: "table-tictactoe"}, 
+          React.createElement("tr", {className: "tictactoe-grid-bottom"}, 
+            React.createElement("td", {className: "tictactoe-grid-right"}, React.createElement("button", {className: "space", onClick: this.play.bind(this, 0, 0)}, this.state.board[0][0])), 
+            React.createElement("td", {className: "tictactoe-grid-right"}, React.createElement("button", {className: "space", onClick: this.play.bind(this, 0, 1)}, this.state.board[0][1])), 
+            React.createElement("td", null, React.createElement("button", {className: "space", onClick: this.play.bind(this, 0, 2)}, this.state.board[0][2]))
+          ), 
+          React.createElement("tr", {className: "tictactoe-grid-bottom"}, 
+            React.createElement("td", {className: "tictactoe-grid-right"}, React.createElement("button", {className: "space", onClick: this.play.bind(this, 1, 0)}, this.state.board[1][0])), 
+            React.createElement("td", {className: "tictactoe-grid-right"}, React.createElement("button", {className: "space", onClick: this.play.bind(this, 1, 1)}, this.state.board[1][1])), 
+            React.createElement("td", null, React.createElement("button", {className: "space", onClick: this.play.bind(this, 1, 2)}, this.state.board[1][2]))
+          ), 
+          React.createElement("tr", null, 
+            React.createElement("td", {className: "tictactoe-grid-right"}, React.createElement("button", {className: "space", onClick: this.play.bind(this, 2, 0)}, this.state.board[2][0])), 
+            React.createElement("td", {className: "tictactoe-grid-right"}, React.createElement("button", {className: "space", onClick: this.play.bind(this, 2, 1)}, this.state.board[2][1])), 
+            React.createElement("td", null, React.createElement("button", {className: "space", onClick: this.play.bind(this, 2, 2)}, this.state.board[2][2]))
+          )
+        )
+      )
+    )
+  }
+});
+
+React.render(React.createElement(TicTacToeBoard, null), document.getElementById('react-container'));
