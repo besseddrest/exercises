ee8d24fd1a6c43b04e915c8e4d825cfd40f3a911
besseddrest <hcabalic@gmail.com>
2016-02-15 13:39:22 -0800

    add styling, remove some uneccessary vars

--- ticTacToe/js/TicTacToe.js
+++ ticTacToe/js/TicTacToe.js
@@ -18,15 +18,16 @@ var TicTacToeBoard = React.createClass({
     this.setState({games: games});
   },
 
-  eachGame: function(game, i) {
+  eachGame: function(game) {
     return (
-      <Game key={game.id} index={i} />
+      <Game key={game.id} />
       );
   },
 
   nextId: function() {
     // keeps track of mmost recent ID
     this.uniqueId = this.uniqueId || 0;
+
     return this.uniqueId++;
   },
 
@@ -57,10 +58,11 @@ var Game = React.createClass({
   },
 
   componentDidMount: function(){
+    // make game draggable (by outer edges)
     $(this.getDOMNode()).draggable();
   },
 
-  play: function(x, y) {
+  play: function(x, y, tag) {
     var board = this.state.board;
 
     // place piece
@@ -70,11 +72,11 @@ var Game = React.createClass({
     this.setState({board: board}, this.checkWinner.bind(this, x, y));
 
     // disable the space on the board
-    //this.disableCell();
+    this.disableCell(tag);
 
   },
 
-  disableCell: function() {
+  disableCell: function(tag) {
     // TODO
     $('button[data-tag="' + tag + '"]').prop('disabled', true);
   },
@@ -131,7 +133,7 @@ var Game = React.createClass({
 
     if (board[1][1] === winner) {
       // check first diagonal
-      if (board[0][0] === winner && board[1][1] === winner) {
+      if (board[0][0] === winner && board[2][2] === winner) {
         return true;
       }
 
@@ -145,7 +147,8 @@ var Game = React.createClass({
   },
 
   _displayWinner: function() {
-    alert('you win');
+    // show that this game has been won
+    $(this.getDOMNode()).addClass('table-success');
   },
 
   render: function() {
@@ -153,19 +156,37 @@ var Game = React.createClass({
       <div className="game">
         <table className="table-tictactoe">
           <tr className="tictactoe-grid-bottom">
-            <td className="tictactoe-grid-right"><button className="space" onClick={this.play.bind(this, 0, 0)}>{this.state.board[0][0]}</button></td>
-            <td className="tictactoe-grid-right"><button className="space" onClick={this.play.bind(this, 0, 1)}>{this.state.board[0][1]}</button></td>
-            <td><button className="space" onClick={this.play.bind(this, 0, 2)}>{this.state.board[0][2]}</button></td>
+            <td className="tictactoe-grid-right">
+              <button className="space" data-tag="topLeft" onClick={this.play.bind(this, 0, 0, 'topLeft')}>{this.state.board[0][0]}</button>
+            </td>
+            <td className="tictactoe-grid-right">
+              <button className="space" data-tag="topMid" onClick={this.play.bind(this, 0, 1, 'topMid')}>{this.state.board[0][1]}</button>
+            </td>
+            <td>
+              <button className="space" data-tag="topRight" onClick={this.play.bind(this, 0, 2, 'topRight')}>{this.state.board[0][2]}</button>
+            </td>
           </tr>
           <tr className="tictactoe-grid-bottom">
-            <td className="tictactoe-grid-right"><button className="space" onClick={this.play.bind(this, 1, 0)}>{this.state.board[1][0]}</button></td>
-            <td className="tictactoe-grid-right"><button className="space" onClick={this.play.bind(this, 1, 1)}>{this.state.board[1][1]}</button></td>
-            <td><button className="space" onClick={this.play.bind(this, 1, 2)}>{this.state.board[1][2]}</button></td>
+            <td className="tictactoe-grid-right">
+              <button className="space" data-tag="midLeft" onClick={this.play.bind(this, 1, 0, 'midLeft')}>{this.state.board[1][0]}</button>
+            </td>
+            <td className="tictactoe-grid-right">
+              <button className="space" data-tag="midMid" onClick={this.play.bind(this, 1, 1, 'midMid')}>{this.state.board[1][1]}</button>
+            </td>
+            <td>
+              <button className="space" data-tag="midRight" onClick={this.play.bind(this, 1, 2, 'midRight')}>{this.state.board[1][2]}</button>
+            </td>
           </tr>
           <tr>
-            <td className="tictactoe-grid-right"><button className="space" onClick={this.play.bind(this, 2, 0)}>{this.state.board[2][0]}</button></td>
-            <td className="tictactoe-grid-right"><button className="space" onClick={this.play.bind(this, 2, 1)}>{this.state.board[2][1]}</button></td>
-            <td><button className="space" onClick={this.play.bind(this, 2, 2)}>{this.state.board[2][2]}</button></td>
+            <td className="tictactoe-grid-right">
+              <button className="space" data-tag="botLeft" onClick={this.play.bind(this, 2, 0, 'botLeft')}>{this.state.board[2][0]}</button>
+            </td>
+            <td className="tictactoe-grid-right">
+              <button className="space" data-tag="botMid" onClick={this.play.bind(this, 2, 1, 'botMid')}>{this.state.board[2][1]}</button>
+            </td>
+            <td>
+              <button className="space" data-tag="botRight" onClick={this.play.bind(this, 2, 2, 'botRight')}>{this.state.board[2][2]}</button>
+            </td>
           </tr>
         </table>
       </div>
