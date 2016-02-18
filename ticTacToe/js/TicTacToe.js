631d76d1de5fc322b55ecdf8b001e0105f85c56e
besseddrest <hcabalic@gmail.com>
2016-02-17 23:18:59 -0800

    getEvenElement

--- ticTacToe/js/TicTacToe.js
+++ ticTacToe/js/TicTacToe.js
@@ -21,11 +21,11 @@ var TicTacToeBoard = React.createClass({
   eachGame: function(game, i) {
     return (
       <Game key={game.id} index={i}/>
-      );
+    );
   },
 
   nextId: function() {
-    // keeps track of mmost recent ID
+    // keeps track of most recent ID
     this.uniqueId = this.uniqueId || 0;
 
     return this.uniqueId++;
@@ -44,16 +44,18 @@ var TicTacToeBoard = React.createClass({
   }
 });
 
+// child of TicTacToeBoard
 var Game = React.createClass({
   getInitialState: function() {
     return {
-      currentPiece: 'X',
-      winner: '',
+      currentPlayer: 'X',
       board: [
         ['','',''],
         ['','',''],
         ['','','']
-      ]
+      ],
+      completed: false,
+      tie: false
     };
   },
 
@@ -62,51 +64,57 @@ var Game = React.createClass({
     $(this.getDOMNode()).draggable();
   },
 
-  play: function(x, y, tag) {
+  play: function(x, y) {
     var board = this.state.board;
 
-    // place piece
-    board[x][y] = this.state.currentPiece;
+    // store current player on 'board'
+    board[x][y] = this.state.currentPlayer;
 
     // set state and check winner
-    this.setState({board: board}, this.checkWinner.bind(this, x, y));
-
-    // disable the space on the board
-    this.disableCell(tag);
-
+    this.setState({board: board}, this._checkWinner.bind(this, x, y));
   },
 
-  disableCell: function(tag) {
-    // TODO
-    $('button[data-tag="' + tag + '"]').prop('disabled', true);
-  },
-
-  _swapPiece: function () {
-    // changes the current player piece
-    if (this.state.currentPiece === 'X') {
-      this.setState({currentPiece: 'O'});
-    } else {
-      this.setState({currentPiece: 'X'});
-    }
-  },
-
-  checkWinner: function(x, y) {
+  _checkWinner: function(x, y) {
     var board = this.state.board;
+    var cases = this._checkRow(x) || this._checkColumn(y) || this._checkDiagonal();
 
-    // check for winner
-    if (this._checkRow(x) || this._checkColumn(y) || this._checkDiagonal()) {
-      this._displayWinner();
+    // keep track of total moves
+    this.moves = this.moves || 1;
+
+    // if we won by one of the cases
+    if (cases) {
+      // mark game as completed
+      this.setState({completed: true});
+      return;
+    }
+
+    // if the game ends in a tie
+    if (this.moves === 9 && !cases) {
+      this._displayTie();
+      return;
+    }
+    
+    // no winner yet, other player's turn
+    this._swapPlayer();
+    this.moves++;
+  },
+
+  _swapPlayer: function () {
+    // changes the current player
+    if (this.state.currentPlayer === 'X') {
+      this.setState({currentPlayer: 'O'});
     } else {
-      this._swapPiece();
+      this.setState({currentPlayer: 'X'});
     }
   },
+
 
   _checkRow: function(x) {
     var board = this.state.board;
 
     // checks all spaces in current row
     for (var i = 0; i < board.length; i++) {
-      if (board[x][i] !== this.state.currentPiece) {
+      if (board[x][i] !== this.state.currentPlayer) {
         return false;
       }
     }
@@ -119,7 +127,7 @@ var Game = React.createClass({
 
     // checks all spaces in current column
     for (var j = 0; j < board.length; j++) {
-      if (board[j][y] !== this.state.currentPiece) {
+      if (board[j][y] !== this.state.currentPlayer) {
         return false;
       }
     }
@@ -129,8 +137,10 @@ var Game = React.createClass({
 
   _checkDiagonal: function() {
     var board = this.state.board;
-    var winner = this.state.currentPiece;
+    var winner = this.state.currentPlayer;
 
+    // we're only dealing with a 3x3 game
+    // so lets check the board explicitly
     if (board[1][1] === winner) {
       // check first diagonal
       if (board[0][0] === winner && board[2][2] === winner) {
@@ -146,50 +156,102 @@ var Game = React.createClass({
     }
   },
 
-  _displayWinner: function() {
-    // show that this game has been won
-    $(this.getDOMNode()).addClass('table-success');
+  _displayTie: function() {
+    this.setState({
+      tie: true
+    });
+  },
+
+  restart: function() {
+    // resetting defaults
+    var board = [
+      ['','',''],
+      ['','',''],
+      ['','','']
+    ];
+
+    this.moves = 1;
+
+    this.setState({
+      board: board,
+      currentPlayer: 'X',
+      completed: false,
+      tie: false
+    });
   },
 
   render: function() {
+    this.gameClasses = 'game';
+    this.restartClasses = 'hidden'
+
+    // completed styling
+    if (this.state.completed) {
+      this.gameClasses = 'game table-success';
+      this.restartClasses = 'btn btn-sm btn-info glyphicon glyphicon-repeat'
+    }
+
+    // tie game styling
+    if (this.state.tie) {
+      this.gameClasses = 'game table-tied';
+      this.restartClasses = 'btn btn-sm btn-info glyphicon glyphicon-repeat'
+    }
+
     return (
-      <div className="game" id={this.props.id}>
+      <div className={this.gameClasses} id={this.props.id}>
         <table className="table-tictactoe">
           <tr className="tictactoe-grid-bottom">
             <td className="tictactoe-grid-right">
-              <button className="space" data-tag="topLeft" onClick={this.play.bind(null, 0, 0, 'topLeft')}>{this.state.board[0][0]}</button>
+              <Space onClick={this.play.bind(null, 0, 0)} value={this.state.board[0][0]} isCompleted={this.state.completed} />
             </td>
             <td className="tictactoe-grid-right">
-              <button className="space" data-tag="topMid" onClick={this.play.bind(null, 0, 1, 'topMid')}>{this.state.board[0][1]}</button>
+              <Space onClick={this.play.bind(null, 0, 1)} value={this.state.board[0][1]} isCompleted={this.state.completed} />
             </td>
             <td>
-              <button className="space" data-tag="topRight" onClick={this.play.bind(null, 0, 2, 'topRight')}>{this.state.board[0][2]}</button>
+              <Space onClick={this.play.bind(null, 0, 2)} value={this.state.board[0][2]} isCompleted={this.state.completed} />
             </td>
           </tr>
           <tr className="tictactoe-grid-bottom">
             <td className="tictactoe-grid-right">
-              <button className="space" data-tag="midLeft" onClick={this.play.bind(null, 1, 0, 'midLeft')}>{this.state.board[1][0]}</button>
+              <Space onClick={this.play.bind(null, 1, 0)} value={this.state.board[1][0]} isCompleted={this.state.completed} />
             </td>
             <td className="tictactoe-grid-right">
-              <button className="space" data-tag="midMid" onClick={this.play.bind(null, 1, 1, 'midMid')}>{this.state.board[1][1]}</button>
+              <Space onClick={this.play.bind(null, 1, 1)} value={this.state.board[1][1]} isCompleted={this.state.completed} />
             </td>
             <td>
-              <button className="space" data-tag="midRight" onClick={this.play.bind(null, 1, 2, 'midRight')}>{this.state.board[1][2]}</button>
+              <Space onClick={this.play.bind(null, 1, 2)} value={this.state.board[1][2]} isCompleted={this.state.completed} />
             </td>
           </tr>
           <tr>
             <td className="tictactoe-grid-right">
-              <button className="space" data-tag="botLeft" onClick={this.play.bind(null, 2, 0, 'botLeft')}>{this.state.board[2][0]}</button>
+              <Space onClick={this.play.bind(null, 2, 0)} value={this.state.board[2][0]} isCompleted={this.state.completed} />
             </td>
             <td className="tictactoe-grid-right">
-              <button className="space" data-tag="botMid" onClick={this.play.bind(null, 2, 1, 'botMid')}>{this.state.board[2][1]}</button>
+              <Space onClick={this.play.bind(null, 2, 1)} value={this.state.board[2][1]} isCompleted={this.state.completed} />
             </td>
             <td>
-              <button className="space" data-tag="botRight" onClick={this.play.bind(null, 2, 2, 'botRight')}>{this.state.board[2][2]}</button>
+              <Space onClick={this.play.bind(null, 2, 2)} value={this.state.board[2][2]} isCompleted={this.state.completed} />
             </td>
           </tr>
         </table>
+        <button onClick={this.restart} className={this.restartClasses} />
       </div>
+    )
+  }
+});
+
+// child of Game
+var Space = React.createClass({
+  render: function() {
+    this.disabled = false;
+
+    // if the space has been played, disable it
+    // if the game is completed, let's disable everything
+    if (this.props.value || this.props.isCompleted) {
+      this.disabled = true;
+    }
+
+    return (
+      <button className="space" onClick={this.props.onClick} disabled={this.disabled}>{this.props.value}</button>
     )
   }
 });
