d80c912cb1974f705b69bb8326136f59ebb70134
besseddrest <hcabalic@gmail.com>
2016-12-29 12:20:53 -0800

    add another closure example

--- js-demos/closure.js
+++ js-demos/closure.js
@@ -1,19 +1,40 @@
-// A closure is a function having access to the parent scope, even after the parent function has closed.
-function fantastic(yourName) {
-  var heroes = ['Reed Richards', 'Susan Storm', 'Johnny Storm', 'Ben Grimm'];
+// A closure is an inner function that has access to its own local variables,
+// outer function's variables, global variables
 
-  return function(name) {
-    if (heroes.indexOf(name) > -1) {
-      console.log('Yes ' + yourName + ', ' + name + ' is in the Fantastic Four.');
-    } else {
-      console.log('No ' + yourName + ', ' + name + ' is on a different team');
-    }
+
+
+
+
+// Function is useless w/o speak()
+function bikeMessage(name, bike) {
+  return 'Hey ' + name + ', that is a great looking ' + bike + '.';
+}
+
+// will this be used by anything other than bikeMessage();
+// we might as well just call speak and pass a message to it
+function speak(message) {
+  console.log(message);
+}
+
+var newMessage = bikeMessage('Andrew', 'LOW//'); // create message
+speak(newMessage);
+
+// Consider a closure:
+function bikeMessage(name, bike) {
+  var message = 'Hey ' + name + ', that is a great looking ' + bike + '.';
+
+  return function speak() { // nice t
+    console.log(message);
   }
 }
 
-var isHero = fantastic('Harold');
-isHero('Wolverine'); // "No Harold, Wolverine is on a different team"
-isHero('Reed Richards'); // "Yes Harold, Reed Richards is in the Fantastic Four"
+var newMessage = bikeMessage('Harold', 'Cannondale'); // create message
+newMessage(); // access to inner function
+
+
+
+
+
 
 // closures can privatize variables
 // self invoking function you can execute code without cluttering global namespace
@@ -31,3 +52,48 @@ var counter = (function(){
 counter();
 counter();
 counter(); // count is 3
+
+
+
+
+
+// not necessarily returning a function
+function makeTimers() {
+  var main = document.getElementById('main');
+  var el1 = document.createElement('div');
+  var el2 = document.createElement('div');
+
+  main.appendChild(el1);
+  main.appendChild(el2);
+
+  // creates two 'timers'
+  addTimer(el1, 200);
+  addTimer(el2, 350);
+}
+
+function addTimer(el, time) {
+  var count = 0;
+  setInterval(function(){count++;}, time);
+  el.innerHTML = 'The count is ' + count;
+}
+
+
+
+
+
+// A closure is a function having access to the parent scope, even after the parent function has closed.
+function fantastic(yourName) {
+  var heroes = ['Reed Richards', 'Susan Storm', 'Johnny Storm', 'Ben Grimm'];
+
+  return function(name) {
+    if (heroes.indexOf(name) > -1) {
+      console.log('Yes ' + yourName + ', ' + name + ' is in the Fantastic Four.');
+    } else {
+      console.log('No ' + yourName + ', ' + name + ' is on a different team');
+    }
+  }
+}
+
+var isHero = fantastic('Harold');
+isHero('Wolverine'); // "No Harold, Wolverine is on a different team"
+isHero('Reed Richards'); // "Yes Harold, Reed Richards is in the Fantastic Four"
